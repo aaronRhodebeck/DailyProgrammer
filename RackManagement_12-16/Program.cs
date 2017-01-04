173c1889d7efb668b60191eb0167846a2c045a1d
aaronRhodebeck <arhodebeck@gmail.com>
2017-01-04 16:31:18 -0600

    Rack Management 1 working no bonus Bonus one is implemented but not working properly at the moment.  All instructions and sample tests written out.

---
+++ RackManagement_12-16/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace RackManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Main Excercise
+            
+            // TestCase 1 -> true
+            var rack1 = new ScrabbleRack("ladilmy");
+            Console.WriteLine(rack1.CanMakeWord("daily"));
+
+            // TestCase 2 -> false
+            var rack2 = new ScrabbleRack("eerriin");
+            Console.WriteLine(rack2.CanMakeWord("eeire"));
+
+            // TestCase 3 -> true
+            var rack3 = new ScrabbleRack("orrpgma");
+            Console.WriteLine(rack3.CanMakeWord("program"));
+
+            // TestCase 4 -> false
+            var rack4 = new ScrabbleRack("orppgma");
+            Console.WriteLine(rack4.CanMakeWord("program"));
+
+            // Bonus 1
+
+            //TestCase 1 -> true
+            var bonusRack1 = new ScrabbleRack("pizza??");
+            Console.WriteLine(bonusRack1.CanMakeWord("pizzazz"));
+
+            //TestCase 2 -> false
+            var bonusRack2 = new ScrabbleRack("piizza?");
+            Console.WriteLine(bonusRack2.CanMakeWord("pizzazz"));
+
+            //TestCase 3 - true
+            var bonusRack3 = new ScrabbleRack("a??????");
+            Console.WriteLine(bonusRack1.CanMakeWord("program"));
+
+            //TestCase 4 - false
+            var bonusRack4 = new ScrabbleRack("b??????");
+            Console.WriteLine(bonusRack1.CanMakeWord("program"));
+
+            // Bonus 2
+
+            //TestCase 1 -> "coauthored"
+            var bonusRack5 = new ScrabbleRack("dcthoyueorza");
+
+            //TestCase 2 -> "turquois"
+            var bonusRack6 = new ScrabbleRack("uruqrnytrois");
+
+            //TestCase 3 - "greengrocery"
+            var bonusRack7 = new ScrabbleRack("rryqeiaegicgeo??");
+
+            //TestCase 4 - "subordinately"
+            var bonusRack8 = new ScrabbleRack("udosjanyuiuebr??");
+
+            // TestCase 5 -> "ovolactovegetarian"
+            var bonusRack9 = new ScrabbleRack("vaakojeaietg????????");
+        }
+    }
+}
---
+++ RackManagement_12-16/RackManagement.cs
@@ -0,0 +1,43 @@
+public class ScrabbleRack
+{
+    private const string PossibleTiles = "abcdefghijklmnopqrstuvwxyz?";
+    public string Tiles { get; set; }
+    private int[] TileFrequency { get; set; }
+    
+    public ScrabbleRack(string tiles)
+    {
+        this.Tiles = tiles.ToLower();
+        this.TileFrequency = SortTiles(tiles);
+    }
+
+    private int[] SortTiles(string tiles)
+    {
+        var tileFrequency = new int[PossibleTiles.Length];
+        foreach (var tile in tiles)
+        {
+            tileFrequency[PossibleTiles.IndexOf(tile)]++;
+        }
+        return tileFrequency;
+    }
+
+    public bool CanMakeWord(string word)
+    {
+        int wildcardsNeeded = 0;        
+        var tilesNeeded = SortTiles(word);
+
+        for (int i = 0; i < tilesNeeded.Length; i++)
+        {
+            if (tilesNeeded[i] > TileFrequency[i])
+            {
+                wildcardsNeeded += tilesNeeded[i] - TileFrequency[i];
+            }
+            
+            if (wildcardsNeeded > TileFrequency[TileFrequency.Length -1])
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
